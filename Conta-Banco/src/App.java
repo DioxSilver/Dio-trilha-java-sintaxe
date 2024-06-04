2224f528776abf12adb7d692c129ca2b162d80dd
wesley <sdwesleysantos@gmail.com>
2024-06-03 22:30:24 -0300

    meu projeto conta banco

---
+++ Conta-Banco/src/App.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println();
+        
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.next();
+
+        System.out.println("agencia: " + agencia);
+
+        System.out.println("Por favor, digite o numero da conta!");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Numero da conta: " + numeroConta);
+
+        System.out.println("Por favor, informe seu nome! ");
+        String nomeCliente = scanner.next();
+        System.out.println("Olá, senhor: " + nomeCliente);
+        
+        System.out.println("Por favor, infome seu saldo");
+        Double saldo = scanner.nextDouble();
+
+        System.out.println("saldo atual " + saldo);
+
+        System.out.println("Olá "+nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta " +numeroConta+" e seu saldo "+ saldo +" já está disponível para saque");
+
+    }
+}
+
---
+++ Conta-Banco/src/Usuario.java
@@ -0,0 +1,11 @@
+public class Usuario {
+
+    public void main(String[]arg) {
+        int numeroConta = 1021;
+        String agencia = "067-8";
+        String nomeCliente= "Mario Andrade";
+        Double Saldo = 237.48;
+
+    }
+    
+}
